2706e63b65b0fdd79fb98c885169f82be9a8c7c7
Sean-mn <b01080080329@gmail.com>
2025-06-30 20:35:15 +0900

    fix: AuthRepository 수정
    - 이름 변경 (AuthRepository -> IAccountRepository)
    - 중복 확인 매서드(CheckExistsAsync) 추가
    
    #1
    #1

--- Server/Server/Repository/Interface/IAccountRepository.cs
+++ Server/Server/Repository/Interface/IAccountRepository.cs
@@ -2,8 +2,9 @@
 
 namespace Server.Repository.Interface;
 
-public interface IAuthRepository
+public interface IAccountRepository
 {
+    Task<bool> CheckExistsAsync(string playerId, string email);
     Task CreatePlayerAccountAsync(PlayerAccountData account);
     Task<PlayerAccountData?> GetByPlayerIdAsync(string id);
     Task<PlayerAccountData?> GetByEmailAsync(string email);
