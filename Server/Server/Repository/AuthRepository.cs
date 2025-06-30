0c7f5fa0842eef24487939ed70e95b5e329075be
Sean-mn <b01080080329@gmail.com>
2025-06-30 20:22:30 +0900

    feat: AuthRepository 생성
    - 계정 Db 연결 리포지토리 생성
    
    #1

---
+++ Server/Server/Repository/AuthRepository.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using MySqlConnector;
+using Server.Model.Account.Entity;
+using Server.Repository.Interface;
+
+namespace Server.Repository;
+
+public class AuthRepository : IAuthRepository
+{
+    private readonly string _connectionString;
+
+    public AuthRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    private MySqlConnection CreateConnection => new(_connectionString);
+    
+    public async Task CreatePlayerAccountAsync(PlayerAccountData account)
+    {
+        const string sql = @"
+                    insert into player_account_data
+                    (id, player_id, player_name, password, email, created_at) 
+                    values
+                    (@Id, @PlayerId, @PlayerName, @Password, @Email, @CreatedAt)";
+
+        await using var connection = CreateConnection;
+        await connection.OpenAsync();
+        await connection.ExecuteAsync(sql, account);
+    }
+
+    public async Task<PlayerAccountData?> GetByPlayerIdAsync(string id)
+    {
+        const string sql = @"
+                    select * 
+                    from player_account_data 
+                    where player_id = @Id
+                    limit 1";
+        
+        await using var connection = CreateConnection;
+        await connection.OpenAsync();
+        return await connection.QueryFirstOrDefaultAsync<PlayerAccountData>(sql, new {Id = id});
+    }
+
+    public async Task<PlayerAccountData?> GetByEmailAsync(string email)
+    {
+        const string sql = @"
+                    select *
+                    from player_account_data
+                    where email = @Email
+                    limit 1";
+        
+        await using var connection = CreateConnection;
+        await connection.OpenAsync();
+        return await connection.QueryFirstOrDefaultAsync<PlayerAccountData>(sql, new {Email = email});
+    }
+}
---
+++ Server/Server/Repository/Interface/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using Server.Model.Account.Entity;
+
+namespace Server.Repository.Interface;
+
+public interface IAuthRepository
+{
+    Task CreatePlayerAccountAsync(PlayerAccountData account);
+    Task<PlayerAccountData?> GetByPlayerIdAsync(string id);
+    Task<PlayerAccountData?> GetByEmailAsync(string email);
+}
