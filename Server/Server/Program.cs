745089d66856b169cf06556568532e5f2a761ff3
Sean-mn <b01080080329@gmail.com>
2025-06-30 20:18:52 +0900

    feat: acount Db 연결 문자열 주입
    #1

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,4 +1,11 @@
+using Server.Repository;
+using Server.Repository.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string accountDbConnectionString = builder.Configuration.GetConnectionString("AccountDb");
+
+builder.Services.AddScoped<IAuthRepository>(sp => new AuthRepository(accountDbConnectionString));
 
 builder.Services.AddControllers();
 
