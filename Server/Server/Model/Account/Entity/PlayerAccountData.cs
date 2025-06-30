bb006e8f8c00635d4960669eebc1fc13d59f0485
Sean-mn <b01080080329@gmail.com>
2025-06-30 20:04:45 +0900

    feat: 플레이어 계정 정보 엔티티 생성
    #1

---
+++ Server/Server/Model/Account/Entity/PlayerAccountData.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Model.Account.Entity;
+
+public class PlayerAccountData
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string PlayerId { get; set; } = string.Empty;
+    public string PlayerName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? LastLoginAt { get; set; }
+}
