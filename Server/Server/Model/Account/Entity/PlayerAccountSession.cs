85465337ee506a120f70140bd9d369aa76d57f4b
Sean-mn <b01080080329@gmail.com>
2025-07-01 01:16:37 +0900

    modify: 이름 수정
    - ExpiresAt  -> ExpiredAt
    
    #1

--- Server/Server/Model/Account/Entity/PlayerAccountSession.cs
+++ Server/Server/Model/Account/Entity/PlayerAccountSession.cs
@@ -5,5 +5,5 @@ public class PlayerAccountSession
     public string SessionId { get; set; } = null!;
     public string AccountId { get; set; } = null!;
     public DateTime CreatedAt { get; set; }
-    public DateTime ExpiresAt { get; set; }
+    public DateTime ExpiredAt { get; set; }
 }
