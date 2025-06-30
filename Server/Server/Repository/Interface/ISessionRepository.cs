4ace4729c68f96eba8557b69a74ab4e90ab40dfa
Sean-mn <b01080080329@gmail.com>
2025-07-01 01:37:49 +0900

    feat: SessionRepository 생성
    #1

---
+++ Server/Server/Repository/Interface/ISessionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Server.Model.Account.Entity;
+
+namespace Server.Repository.Interface;
+
+public interface ISessionRepository
+{
+    Task<string> CreateSessionAsync(PlayerAccountSession session);
+    Task<bool> HasValidSessionAsync(string sessionId);
+}
---
+++ Server/Server/Repository/SessionRepository.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using MySqlConnector;
+using Server.Model.Account.Entity;
+using Server.Repository.Interface;
+
+namespace Server.Repository;
+
+public class SessionRepository : ISessionRepository
+{
+    private readonly string _connectionString;
+
+    public SessionRepository(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("AccountDb");
+    }
+    private MySqlConnection CreateConnection() => new(_connectionString);
+    
+    public async Task<string> CreateSessionAsync(PlayerAccountSession session)
+    {
+        const string sql = @"
+                    INSERT INTO player_account_session (session_id, account_id, created_at, expired_at)
+                    VALUES (@SessionId, @AccountId, @CreatedAt, @ExpiredAt);
+                    ";
+
+        await using var connection = CreateConnection();
+        await connection.OpenAsync();
+
+        await connection.ExecuteAsync(sql, new
+        {
+            session.SessionId,
+            session.AccountId,
+            session.CreatedAt,
+            session.ExpiredAt
+        });
+
+        return session.SessionId;
+    }
+
+    public async Task<bool> HasValidSessionAsync(string sessionId)
+    {
+        const string sql = @"
+                    SELECT COUNT(1)
+                    FROM player_account_session
+                    WHERE session_id = @SessionId
+                    AND expired_at > NOW();
+                    ";
+        
+        await using var connection = CreateConnection();
+        await connection.OpenAsync();
+        
+        var count = await connection.ExecuteScalarAsync<int>(sql, new { SessionId = sessionId });
+        return count > 0;
+    }
+}
