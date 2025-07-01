45d59a4401ccd6884f0f3bfea26bd89f7411219a
Sean-mn <b01080080329@gmail.com>
2025-07-01 21:11:12 +0900

    feat: RankingService 생성
    #3

---
+++ Server/Server/Service/Interface/IRankingService.cs
@@ -0,0 +1,11 @@
+﻿using Server.Model.Data.Entity;
+
+namespace Server.Service.Interface;
+
+public interface IRankingService
+{
+    Task<IEnumerable<PlayerRankingData>> GetAllRankingsAsync();
+    Task<PlayerRankingData?> GetRankingByPlayerIdAsync(string playerId);
+    Task AddPlayerRankingAsync(PlayerRankingData data);
+    Task UpdatePlayerScoreAsync(string playerId, int newScore);
+}
---
+++ Server/Server/Service/RankingService.cs
@@ -0,0 +1,35 @@
+﻿using Server.Model.Data.Entity;
+using Server.Repository.Interface;
+using Server.Service.Interface;
+
+namespace Server.Service;
+
+public class RankingService : IRankingService
+{
+    private readonly IRankingRepository  _repository;
+
+    public RankingService(IRankingRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task<IEnumerable<PlayerRankingData>> GetAllRankingsAsync()
+    {
+        return await _repository.GetAllRankingDataAsync();
+    }
+
+    public async Task<PlayerRankingData?> GetRankingByPlayerIdAsync(string playerId)
+    {
+        return await _repository.GetRankingDataAsync(playerId);
+    }
+
+    public async Task AddPlayerRankingAsync(PlayerRankingData data)
+    {
+        await _repository.AddPlayerRankingAsync(data);
+    }
+
+    public async Task UpdatePlayerScoreAsync(string playerId, int newScore)
+    {
+        await _repository.UpdatePlayerRankingAsync(playerId, newScore);
+    }
+}
