eea13f26b7ce33333d2ffc8103f02998ef3e7c9b
Sean-mn <b01080080329@gmail.com>
2025-07-01 11:15:31 +0900

    feat: 서비스/리포지토리 DI
    #1

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,4 +1,13 @@
+using Server.Repository;
+using Server.Repository.Interface;
+using Server.Service;
+using Server.Service.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<ISessionRepository, SessionRepository>();
 
 builder.Services.AddControllers();
 
