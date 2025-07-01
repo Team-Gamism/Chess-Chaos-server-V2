2b7c83cf8b6d17f5b7ee6a0834dc7f130daf0ef9
Sean-mn <b01080080329@gmail.com>
2025-07-01 09:55:11 +0900

    feat: 플레이어 회원가입 Dto 생성
    #1

---
+++ Server/Server/Model/Account/Dto/Request/PlayerRegisterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Model.Account.Dto.Request;
+
+public class PlayerRegisterRequest
+{
+    public string PlayerId { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
---
+++ Server/Server/Model/Account/Dto/Response/PlayerRegisterResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.Model.Account.Dto.Response;
+
+public class PlayerRegisterResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string PlayerId { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PlayerName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
