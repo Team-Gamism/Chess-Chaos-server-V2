15680b905533b70540aa1860f0d5b4a272b19211
Sean-mn <b01080080329@gmail.com>
2025-07-01 09:57:11 +0900

    feat: 플레이어 로그인 Dto 생성
    #1

---
+++ Server/Server/Model/Account/Dto/Response/PlayerLoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Model.Account.Dto.Response;
+
+public class PlayerLoginResponse
+{
+    public string PlayerId { get; set; } = null!;
+    public string PlayerName { get; set; } = null!;
+    public string SessionId { get; set; } = null!;
+}
