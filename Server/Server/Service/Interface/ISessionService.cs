e7d39fe2068165c8bae405b1b21eecc75143a9b3
Sean-mn <b01080080329@gmail.com>
2025-07-01 11:28:30 +0900

    feat: SessionService 생성
    #1

---
+++ Server/Server/Service/Interface/ISessionService.cs
@@ -0,0 +1,11 @@
+﻿using Server.Model.Account.Entity;
+
+namespace Server.Service.Interface;
+
+public interface ISessionService
+{
+    Task<string> CreateSessionAsync(string accountId);
+    Task<PlayerAccountSession?> GetSessionAsync(string sessionId);
+    Task<bool> ValidateSessionAsync(string sessionId);
+    Task<bool> ExpireSessionAsync(string sessionId);
+}
---
+++ Server/Server/Service/SessionService.cs
@@ -0,0 +1,43 @@
+﻿using Server.Model.Account.Entity;
+using Server.Repository.Interface;
+using Server.Service.Interface;
+
+namespace Server.Service;
+
+public class SessionService : ISessionService
+{
+    private readonly ISessionRepository _sessionRepository;
+
+    public SessionService(ISessionRepository sessionRepository)
+    {
+        _sessionRepository = sessionRepository;
+    }
+    
+    public async Task<string> CreateSessionAsync(string accountId)
+    {
+        var session = new PlayerAccountSession
+        {
+            AccountId = accountId,
+            SessionId = Guid.NewGuid().ToString(),
+            CreatedAt = DateTime.UtcNow,
+            ExpiredAt = DateTime.UtcNow.AddHours(1)
+        };
+        
+        return await _sessionRepository.CreateSessionAsync(session);
+    }
+
+    public async Task<PlayerAccountSession?> GetSessionAsync(string sessionId)
+    {
+        return await _sessionRepository.GetSessionAsync(sessionId);
+    }
+
+    public async Task<bool> ValidateSessionAsync(string sessionId)
+    {
+        return await _sessionRepository.HasValidSessionAsync(sessionId);
+    }
+
+    public async Task<bool> ExpireSessionAsync(string sessionId)
+    {
+        return await _sessionRepository.ExpireSessionAsync(sessionId);
+    }
+}
