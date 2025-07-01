abfe3a17c4cd6259b2855cd141dbd198599fee56
Sean-mn <b01080080329@gmail.com>
2025-07-01 20:38:12 +0900

    feat: 플레이어 랭킹 데이터 엔티티 생성
    #3

---
+++ Server/Server/Model/Data/Entity/PlayerRankingData.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Model.Data.Entity;
+
+public class PlayerRankingData
+{
+    public int Ranking { get; set; }
+    public string PlayerId { get; set; } = null!;
+    public int PlayerScore { get; set; }
+}
