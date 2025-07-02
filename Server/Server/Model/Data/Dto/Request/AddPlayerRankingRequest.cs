5ea992b7cfddd7289fff78e0b257d216e58a0fc5
Sean-mn <b01080080329@gmail.com>
2025-07-02 15:37:24 +0900

    feat: 플레이어 랭킹 추가 Dto 생성
    - AddPlayerRankingRequest Dto 생성
    - AddPlayerRankingResponse Dto 생성
    
    #3

---
+++ Server/Server/Model/Data/Dto/Request/AddPlayerRankingRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Model.Data.Dto.Request;
+
+public class AddPlayerRankingRequest
+{
+    public string PlayerId { get; set; } = null!;
+    public int PlayerScore { get; set; }
+}
---
+++ Server/Server/Model/Data/Dto/Response/AddPlayerRankingResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Model.Data.Dto.Response;
+
+public class AddPlayerRankingResponse
+{
+    public string PlayerId { get; set; } = null!;
+    public int PlayerScore { get; set; }
+    public int Ranking { get; set; }
+}
