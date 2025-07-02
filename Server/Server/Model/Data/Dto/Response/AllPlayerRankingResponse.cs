e18861d474a41f97c0f2626cb6cd80be0ff98578
Sean-mn <b01080080329@gmail.com>
2025-07-02 16:08:25 +0900

    feat: 플레이어 랭킹 조회 Dto 생성
    - 전체 조회 Dto (AllPlayerRankingResponse)
    - 플레이어 Id로 조회 (PlayerRankingResponse)

---
+++ Server/Server/Model/Data/Dto/Response/AllPlayerRankingResponse.cs
@@ -0,0 +1,9 @@
+﻿using Server.Model.Data.Entity;
+
+namespace Server.Model.Data.Dto.Response;
+
+public class AllPlayerRankingResponse
+{
+    public int Count { get; set; }
+    public List<PlayerRankingData> Rankings { get; set; } = new();
+}
---
+++ Server/Server/Model/Data/Dto/Response/PlayerRankingResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Model.Data.Dto.Response;
+
+public class PlayerRankingResponse
+{
+    public string PlayerId { get; set; } = null!;
+    public int Ranking { get; set; }
+    public int PlayerScore { get; set; }
+}
